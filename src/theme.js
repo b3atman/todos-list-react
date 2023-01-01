dc933bf16afb5f9f80be25902d858f0d8e0cd570
Hubert Ludwiczak <ludwiczakhubert@gmail.com>
2023-01-01 19:35:12 +0100

    creating theme.js

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  color: {
+    black: "#000",
+    crimson: "#DC143C",
+    greenHaze: "#01A368",
+    white: "#fff",
+    teal: "#008080",
+    silver: "#5C0120",
+  },
+  breakpoint: {
+    mobile: 767,
+  },
+};
