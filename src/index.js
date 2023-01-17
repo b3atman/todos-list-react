3631a6cbd77e72d33fe9cece0708bea0da7166e6
Hubert Ludwiczak <ludwiczakhubert@gmail.com>
2023-01-17 19:11:45 +0100

    adding Provider and store

--- src/index.js
+++ src/index.js
@@ -1,60 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from './GlobalStyle';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import { theme } from "./theme";
-
-import { configureStore } from "@reduxjs/toolkit";
-
-const InitialState = {
-  tasks: [],
-};
-
-const tasksReducer = (state = InitialState, action) => {
-  switch (action.type) {
-    case "addTask":
-      return {
-        ...state,
-        tasks: [
-          ...state.tasks,
-          {
-            content: action.payload,
-          },
-        ],
-      };
-
-    default:
-      return state;
-  }
-};
-
-const addTask = content => ({
-  type: "addTask",
-  payload: content,
-});
-
-const selectTasks = ({ tasks }) => tasks;
-
-const store = configureStore({ reducer: tasksReducer });
-console.log(selectTasks(store.getState()));
-
-store.dispatch(addTask("zadanie 1"));
-
-console.log(selectTasks(store.getState()));
-
-store.dispatch(addTask("zadanie 2"));
-
-console.log(selectTasks(store.getState()));
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
