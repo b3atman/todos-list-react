147ed7f018fcad2926ffcf7a83fbb52dc21690ab
Hubert Ludwiczak <ludwiczakhubert@gmail.com>
2023-02-12 11:11:38 +0100

    udpate file

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -41,7 +41,7 @@ const selectTasksState = state => state.tasks;
 
 export const selectTasks = state => selectTasksState(state).tasks;
 export const selectHideDone = state => selectTasksState(state).hideDone;
-export const selectAreTasksEmpty = state => selectTasksState(state).length === 0;
+export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
 
 export default tasksSlice.reducer;
